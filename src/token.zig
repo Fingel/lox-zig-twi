04cd431a5bfbc80a387a4dd7d368cb09212810ea
Austin Riba <austin@m51.io>
2025-05-19 21:44:59 -0700

    Format token file correctly

--- src/token.zig
+++ src/token.zig
@@ -1,53 +1,22 @@
 const std = @import("std");
 
 pub const TokenType = enum {
+    // zig fmt: off
+
     // Single-character tokens
-    LEFT_PAREN,
-    RIGHT_PAREN,
-    LEFT_BRACE,
-    RIGHT_BRACE,
-    COMMA,
-    DOT,
-    MINUS,
-    PLUS,
-    SEMICOLON,
-    SLASH,
-    STAR,
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE, COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
 
     // One or two character tokens
-    BANG,
-    BANG_EQUAL,
-    EQUAL,
-    EQUAL_EQUAL,
-    GREATER,
-    GREATER_EQUAL,
-    LESS,
-    LESS_EQUAL,
+    BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL, GREATER, GREATER_EQUAL, LESS, LESS_EQUAL,
 
     // Literals
-    IDENTIFIER,
-    STRING,
-    NUMBER,
+    IDENTIFIER, STRING, NUMBER,
 
     // Keywords
-    AND,
-    CLASS,
-    ELSE,
-    FALSE,
-    FUN,
-    FOR,
-    IF,
-    NIL,
-    OR,
-    PRINT,
-    RETURN,
-    SUPER,
-    THIS,
-    TRUE,
-    VAR,
-    WHILE,
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR, PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
 
     EOF,
+    // zig fmt: on
 };
 
 pub const Token = struct {
